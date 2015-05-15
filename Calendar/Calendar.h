21e7340a92ea9f523618dc435d955c8f4f70aa1f
Eike Bartels <eike.t.bartels@gmail.com>
2015-05-15 20:10:53 +0800

    added Calendar framework

---
+++ Calendar/Calendar.h
@@ -0,0 +1,19 @@
+//
+//  Calendar.h
+//  Calendar
+//
+//  Created by Eike Bartels on 15/05/2015.
+//  Copyright (c) 2015 Eike Bartels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Calendar.
+FOUNDATION_EXPORT double CalendarVersionNumber;
+
+//! Project version string for Calendar.
+FOUNDATION_EXPORT const unsigned char CalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calendar/PublicHeader.h>
+
+
