bae64e581c781db6996bbd475aa2336e95068374
Eike Bartels <eike.t.bartels@gmail.com>
2015-05-16 18:20:12 +0800

    added weekdays

--- Calendar/CalendarView.swift
+++ Calendar/CalendarView.swift
@@ -11,6 +11,8 @@ import UIKit
 @IBDesignable
 public class CalendarView: UIView {
 
+    // MARK: - Properties
+    
     let numberOfDays = 7
     var rows:[CalendarRow] = []
     
@@ -19,12 +21,15 @@ public class CalendarView: UIView {
 //        super.init(coder: aDecoder)
 //    }
     
-    override public func didMoveToSuperview() {
-    
-        var previewView:CalendarRow?
 
+    override public func didMoveToSuperview() {
+        
+        var previewView:CalendarRow?
+        var weekdays = self.weekdays()
+        
         for (var i = 0; i < numberOfDays; i++){
-            var row:CalendarRow = CalendarRow(index: i, title:"bla")
+            var row:CalendarRow = CalendarRow(index: i, title:weekdays[i])
+            self.rows.append(row)
             self.addSubview(row)
             
             if let previewView = previewView{
@@ -42,6 +47,30 @@ public class CalendarView: UIView {
             previewView = row
         }
 
+    }
+    
+    private func weekdays() -> [String]{
+        // get Weekdays name
+        let fmt = NSDateFormatter()
+        var days:[String] = fmt.shortWeekdaySymbols as! [String]
+        
+        // replace current weekday with today
+        fmt.dateFormat = "EE"
+        var currentDay = fmt.stringFromDate(NSDate())
+        for (var i = 0; i < days.count; i++){
+            if(days[i] == currentDay){
+                days[i] = "today"
+                break
+            }
+        }
+        
+        // correct first day of the week
+        let firstDay = days.first
+        days.removeAtIndex(0)
+        days.append(firstDay!)
+
+
+        return days
     }
     
     // MARK: - Constraints
